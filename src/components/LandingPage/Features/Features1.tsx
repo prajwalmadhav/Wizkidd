7412b45814699460f4bb0da636182b69fff75b5a
PG <prajwalsmadhav@gmail.com>
2022-10-18 23:33:08 +0530

    feature 1 updated with proepr palcements

--- src/components/LandingPage/Features/Features1.tsx
+++ src/components/LandingPage/Features/Features1.tsx
@@ -8,6 +8,7 @@ import {
     Image,
     HStack,
     Grid,
+    Flex,
   } from '@chakra-ui/react';
   
   const IMAGE1 =
@@ -22,262 +23,266 @@ import {
   export default function ProductSimple() {
     return (
         
-    <HStack spacing={35} pl={35} pt={60} pb={58}> 
+        <Flex mt={{lg:10}} ml={{lg:4}} mb={{lg:60}} bg={useColorModeValue('white', 'gray.800')}>
+            <HStack  spacing={35} pl={35} pt={60} pb={58} bg={useColorModeValue('white', 'gray.800')}> 
+            
+            <Box
+                role={'group'}
+                p={6}
+                isInline
+                maxW={'330px'}
+                w={'full'}
+                bg={useColorModeValue('white', 'gray.800')}
+                boxShadow={'2xl'}
+                rounded={'lg'}
+                pos={'relative'}
+                
+                zIndex={1}>
+                <Box
+                    rounded={'lg'}
+                    mt={-12}
+                    pos={'relative'}
+                    height={'230px'}
+                    
+                    _after={{
+                    transition: 'all .3s ease',
+                    content: '""',
+                    w: 'full',
+                    h: 'full',
+                    pos: 'absolute',
+                    top: 5,
+                    left: 2,
+                    
+                    backgroundImage: `url(${IMAGE1})`,
+                    filter: 'blur(35px)',
+                    zIndex: -1,
+                    
+                    }}
+                    _groupHover={{
+                    _after: {
+                        filter: 'blur(30px)',
+                    },
+                    }}>
+                    <Image
+                    rounded={'lg'}
+                    height={230}
+                    width={282}
+                    objectFit={'cover'}
+                    src={IMAGE1}
+                    />
+                </Box>
+                <Stack pt={10} align={'center'}>
+                    <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
+                    {/* tag line comes here  */}
+                    </Text>
+                    <Heading fontSize={'3xl'} fontFamily={'body'} fontWeight={500}>
+                    Feature 1
+                    </Heading>
+                    <Stack direction={'row'} align={'center'}>
+                    <Text fontWeight={800} fontSize={'xl'}>
+                        {/* Text comes here  */}
+                    </Text>
+                    <Text textDecoration={'line-through'} color={'gray.600'}>
+                        
+                    </Text>
+                    </Stack>
+                </Stack>
+            </Box>
+
+                {/* box 2 */}
+            <Box
+                role={'group'}
+                p={6}
+                isInline
+                maxW={'330px'}
+                w={'full'}
+                bg={useColorModeValue('white', 'gray.800')}
+                boxShadow={'2xl'}
+                rounded={'lg'}
+                pos={'relative'}
+                
+                zIndex={1}>
+                <Box
+                    rounded={'lg'}
+                    mt={-12}
+                    pos={'relative'}
+                    height={'230px'}
+                    
+                    _after={{
+                    transition: 'all .3s ease',
+                    content: '""',
+                    w: 'full',
+                    h: 'full',
+                    pos: 'absolute',
+                    top: 5,
+                    left: 2,
+                    
+                    backgroundImage: `url(${IMAGE2})`,
+                    filter: 'blur(30px)',
+                    zIndex: -1,
+                    
+                    }}
+                    _groupHover={{
+                    _after: {
+                        filter: 'blur(20px)',
+                    },
+                    }}>
+                    <Image
+                    rounded={'lg'}
+                    height={230}
+                    width={282}
+                    objectFit={'cover'}
+                    src={IMAGE2}
+                    />
+                </Box>
+                <Stack pt={10} align={'center'}>
+                    <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
+                    {/* tag line comes here  */}
+                    </Text>
+                    <Heading fontSize={'3xl'} fontFamily={'body'} fontWeight={500}>
+                    Feature 2
+                    </Heading>
+                    <Stack direction={'row'} align={'center'}>
+                    <Text fontWeight={800} fontSize={'xl'}>
+                        {/* Text comes here  */}
+                    </Text>
+                    <Text textDecoration={'line-through'} color={'gray.600'}>
+                        
+                    </Text>
+                    </Stack>
+                </Stack>
+            </Box>
+                {/* box 3 */}
+                <Box
+                role={'group'}
+                p={6}
+                maxW={'330px'}
+                w={'full'}
+                bg={useColorModeValue('white', 'gray.800')}
+                boxShadow={'2xl'}
+                rounded={'lg'}
+                pos={'relative'}
+                isInline
+                zIndex={1}>
+                <Box
+                    rounded={'lg'}
+                    mt={-12}
+                    pos={'relative'}
+                    height={'230px'}
+                    
+                    _after={{
+                    transition: 'all .3s ease',
+                    content: '""',
+                    w: 'full',
+                    h: 'full',
+                    pos: 'absolute',
+                    top: 5,
+                    left: 2,
+                    
+                    backgroundImage: `url(${IMAGE3})`,
+                    filter: 'blur(30px)',
+                    zIndex: -1,
+                    
+                    }}
+                    _groupHover={{
+                    _after: {
+                        filter: 'blur(20px)',
+                    },
+                    }}>
+                    <Image
+                    rounded={'lg'}
+                    height={230}
+                    width={282}
+                    objectFit={'cover'}
+                    src={IMAGE3}
+                    />
+                </Box>
+                <Stack pt={10} align={'center'}>
+                    <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
+                    {/* tag line comes here  */}
+                    </Text>
+                    <Heading fontSize={'3xl'} fontFamily={'body'} fontWeight={500}>
+                    Feature 3
+                    </Heading>
+                    <Stack direction={'row'} align={'center'}>
+                    <Text fontWeight={800} fontSize={'xl'}>
+                        {/* Text comes here  */}
+                    </Text>
+                    <Text textDecoration={'line-through'} color={'gray.600'}>
+                        
+                    </Text>
+                    </Stack>
+                </Stack>
+            </Box>
+
+            {/* Box 4 comes here */}
+
+            <Box
+                role={'group'}
+                p={6}
+                maxW={'330px'}
+                w={'full'}
+                bg={useColorModeValue('white', 'gray.800')}
+                boxShadow={'2xl'}
+                rounded={'lg'}
+                pos={'relative'}
+                isInline
+                zIndex={1}>
+                <Box
+                    rounded={'lg'}
+                    mt={-12}
+                    pos={'relative'}
+                    height={'230px'}
+                    
+                    _after={{
+                    transition: 'all .3s ease',
+                    content: '""',
+                    w: 'full',
+                    h: 'full',
+                    pos: 'absolute',
+                    top: 5,
+                    left: 2,
+                    
+                    backgroundImage: `url(${IMAGE4})`,
+                    filter: 'blur(30px)',
+                    zIndex: -1,
+                    
+                    }}
+                    _groupHover={{
+                    _after: {
+                        filter: 'blur(20px)',
+                    },
+                    }}>
+                    <Image
+                    rounded={'lg'}
+                    height={230}
+                    width={282}
+                    objectFit={'cover'}
+                    src={IMAGE4}
+                    />
+                </Box>
+                <Stack pt={10} align={'center'}>
+                    <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
+                    {/* tag line comes here  */}
+                    </Text>
+                    <Heading fontSize={'3xl'} fontFamily={'body'} fontWeight={500}>
+                    Feature 4 
+                    </Heading>
+                    <Stack direction={'row'} align={'center'}>
+                    <Text fontWeight={800} fontSize={'xl'}>
+                        {/* Text comes here  */}
+                    </Text>
+                    <Text textDecoration={'line-through'} color={'gray.600'}>
+                        
+                    </Text>
+                    </Stack>
+                </Stack>
+            </Box>
         
-        <Box
-            role={'group'}
-            p={6}
-            isInline
-            maxW={'330px'}
-            w={'full'}
-            bg={useColorModeValue('white', 'gray.800')}
-            boxShadow={'2xl'}
-            rounded={'lg'}
-            pos={'relative'}
-            
-            zIndex={1}>
-            <Box
-                rounded={'lg'}
-                mt={-12}
-                pos={'relative'}
-                height={'230px'}
-                
-                _after={{
-                transition: 'all .3s ease',
-                content: '""',
-                w: 'full',
-                h: 'full',
-                pos: 'absolute',
-                top: 5,
-                left: 2,
-                
-                backgroundImage: `url(${IMAGE1})`,
-                filter: 'blur(30px)',
-                zIndex: -1,
-                
-                }}
-                _groupHover={{
-                _after: {
-                    filter: 'blur(20px)',
-                },
-                }}>
-                <Image
-                rounded={'lg'}
-                height={230}
-                width={282}
-                objectFit={'cover'}
-                src={IMAGE1}
-                />
-            </Box>
-            <Stack pt={10} align={'center'}>
-                <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-                {/* tag line comes here  */}
-                </Text>
-                <Heading fontSize={'3xl'} fontFamily={'body'} fontWeight={500}>
-                Adaptive Learning 
-                </Heading>
-                <Stack direction={'row'} align={'center'}>
-                <Text fontWeight={800} fontSize={'xl'}>
-                    {/* Text comes here  */}
-                </Text>
-                <Text textDecoration={'line-through'} color={'gray.600'}>
-                    
-                </Text>
-                </Stack>
-            </Stack>
-        </Box>
-
-            {/* box 2 */}
-        <Box
-            role={'group'}
-            p={6}
-            isInline
-            maxW={'330px'}
-            w={'full'}
-            bg={useColorModeValue('white', 'gray.800')}
-            boxShadow={'2xl'}
-            rounded={'lg'}
-            pos={'relative'}
-            
-            zIndex={1}>
-            <Box
-                rounded={'lg'}
-                mt={-12}
-                pos={'relative'}
-                height={'230px'}
-                
-                _after={{
-                transition: 'all .3s ease',
-                content: '""',
-                w: 'full',
-                h: 'full',
-                pos: 'absolute',
-                top: 5,
-                left: 2,
-                
-                backgroundImage: `url(${IMAGE2})`,
-                filter: 'blur(30px)',
-                zIndex: -1,
-                
-                }}
-                _groupHover={{
-                _after: {
-                    filter: 'blur(20px)',
-                },
-                }}>
-                <Image
-                rounded={'lg'}
-                height={230}
-                width={282}
-                objectFit={'cover'}
-                src={IMAGE2}
-                />
-            </Box>
-            <Stack pt={10} align={'center'}>
-                <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-                {/* tag line comes here  */}
-                </Text>
-                <Heading fontSize={'3xl'} fontFamily={'body'} fontWeight={500}>
-                Performance Report
-                </Heading>
-                <Stack direction={'row'} align={'center'}>
-                <Text fontWeight={800} fontSize={'xl'}>
-                    {/* Text comes here  */}
-                </Text>
-                <Text textDecoration={'line-through'} color={'gray.600'}>
-                    
-                </Text>
-                </Stack>
-            </Stack>
-        </Box>
-            {/* box 3 */}
-            <Box
-            role={'group'}
-            p={6}
-            maxW={'330px'}
-            w={'full'}
-            bg={useColorModeValue('white', 'gray.800')}
-            boxShadow={'2xl'}
-            rounded={'lg'}
-            pos={'relative'}
-            isInline
-            zIndex={1}>
-            <Box
-                rounded={'lg'}
-                mt={-12}
-                pos={'relative'}
-                height={'230px'}
-                
-                _after={{
-                transition: 'all .3s ease',
-                content: '""',
-                w: 'full',
-                h: 'full',
-                pos: 'absolute',
-                top: 5,
-                left: 2,
-                
-                backgroundImage: `url(${IMAGE3})`,
-                filter: 'blur(30px)',
-                zIndex: -1,
-                
-                }}
-                _groupHover={{
-                _after: {
-                    filter: 'blur(20px)',
-                },
-                }}>
-                <Image
-                rounded={'lg'}
-                height={230}
-                width={282}
-                objectFit={'cover'}
-                src={IMAGE3}
-                />
-            </Box>
-            <Stack pt={10} align={'center'}>
-                <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-                {/* tag line comes here  */}
-                </Text>
-                <Heading fontSize={'3xl'} fontFamily={'body'} fontWeight={500}>
-                Personal Content
-                </Heading>
-                <Stack direction={'row'} align={'center'}>
-                <Text fontWeight={800} fontSize={'xl'}>
-                    {/* Text comes here  */}
-                </Text>
-                <Text textDecoration={'line-through'} color={'gray.600'}>
-                    
-                </Text>
-                </Stack>
-            </Stack>
-        </Box>
-
-        {/* Box 4 comes here */}
-
-        <Box
-            role={'group'}
-            p={6}
-            maxW={'330px'}
-            w={'full'}
-            bg={useColorModeValue('white', 'gray.800')}
-            boxShadow={'2xl'}
-            rounded={'lg'}
-            pos={'relative'}
-            isInline
-            zIndex={1}>
-            <Box
-                rounded={'lg'}
-                mt={-12}
-                pos={'relative'}
-                height={'230px'}
-                
-                _after={{
-                transition: 'all .3s ease',
-                content: '""',
-                w: 'full',
-                h: 'full',
-                pos: 'absolute',
-                top: 5,
-                left: 2,
-                
-                backgroundImage: `url(${IMAGE4})`,
-                filter: 'blur(30px)',
-                zIndex: -1,
-                
-                }}
-                _groupHover={{
-                _after: {
-                    filter: 'blur(20px)',
-                },
-                }}>
-                <Image
-                rounded={'lg'}
-                height={230}
-                width={282}
-                objectFit={'cover'}
-                src={IMAGE4}
-                />
-            </Box>
-            <Stack pt={10} align={'center'}>
-                <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-                {/* tag line comes here  */}
-                </Text>
-                <Heading fontSize={'3xl'} fontFamily={'body'} fontWeight={500}>
-                Realtime Feedback 
-                </Heading>
-                <Stack direction={'row'} align={'center'}>
-                <Text fontWeight={800} fontSize={'xl'}>
-                    {/* Text comes here  */}
-                </Text>
-                <Text textDecoration={'line-through'} color={'gray.600'}>
-                    
-                </Text>
-                </Stack>
-            </Stack>
-        </Box>
-      
-      </HStack> 
+            </HStack>
+        </Flex>
+        
+     
     );
   }
   
