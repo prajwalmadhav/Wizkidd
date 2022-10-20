ed2d4bc594607248b2f737b305c6396fa07b307c
PG <prajwalsmadhav@gmail.com>
2022-10-21 01:22:16 +0530

    added a new section in lp

--- src/components/LandingPage/Features/Features5.tsx
+++ src/components/LandingPage/Features/Features5.tsx
@@ -1,96 +1,127 @@
-import React from "react";
-import {
-  chakra,
-  Box,
-  useColorModeValue,
-  Flex,
-  SimpleGrid,
-  Icon,
-  Image,
-  Link,
-} from "@chakra-ui/react";
+import { Box, Icon, chakra, Flex, SimpleGrid } from "@chakra-ui/react";
+import { ReactChild, ReactFragment, ReactPortal } from "react";
 
-import { FiExternalLink } from "react-icons/fi";
-
-const CTA = () => {
-  return (
-    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={0} py={{base:10,lg:40}}>
-      <Flex bg="brand.400">
-        <Image
-          src="https://www.gurooproducer.com/hubfs/resources2.png"
-          alt="How it works illustration"
-          fit="cover"
-          w="full"
-          h={{ base: 64, md: "full" }}
-          ml={{ base: 2, lg: 5 }}
-          // bg="gray.100"
-          loading="lazy"
-          opacity={1}
-        />
-      </Flex>
-      <Flex
-        direction="column"
-        alignItems="start"
-        justifyContent="center"
-        px={{ base: 4, md: 8, lg: 20 }}
-        py={24}
-        zIndex={3}
-      >
-        <chakra.span
-          color={useColorModeValue("brand.600", "gray.300")}
-          fontSize="lg"
-          textTransform="uppercase"
-          fontWeight="extrabold"
-        >
-          How it works ?
-        </chakra.span>
-        <chakra.h1
+export default function Fg()  {
+  const Feature = (props: { icon: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; title: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; children: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; }) => {
+    return (
+      <Box >
+        <Icon
+          boxSize={12}
+          _light={{
+            color: "brand.700",
+          }}
           mb={4}
-          fontSize={{ base: "4xl", md: "4xl", lg: "5xl" }}
-          fontWeight="medium"
-          color={useColorModeValue("brand.600", "gray.300")}
-          lineHeight="auto"
-          textShadow="1px 0 currentcolor"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
         >
-           specific title [doctor] {/*//We&apos;re here to help */}
-        </chakra.h1>
+          {props.icon}
+        </Icon>
+        <chakra.h3
+          mb={3}
+          fontSize="lg"
+          lineHeight="shorter"
+          fontWeight="bold"
+          _light={{
+            color: "gray.900",
+          }}
+        >
+          {props.title}
+        </chakra.h3>
         <chakra.p
-          pr={{ base: 0, lg: 16 }}
-          mb={4}
-          fontSize="lg"
-          color={useColorModeValue("brand.600", "gray.400")}
-          letterSpacing="wider"
-          textAlign='justify'
+          lineHeight="tall"
+          color="gray.600"
+          _dark={{
+            color: "gray.400",
+          }}
         >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          {props.children}
         </chakra.p>
-        <Box display="inline-flex" rounded="md" shadow="md">
-          <chakra.a
-            mt={2}
-            display="inline-flex"
-            alignItems="center"
-            justifyContent="center"
-            px={5}
-            py={3}
-            border="solid transparent"
-            fontWeight="bold"
-            w="full"
-            rounded="md"
-            color={useColorModeValue("black","black")}
-            bg={useColorModeValue("powderblue", "powderblue")}
-            _hover={{
-              bg: useColorModeValue("brand.700", "brand.600"),
-            }}
-          >
-             
-            
-            <Link href={'#'}>Sign Up to Get Started</Link>
-            <Icon as={FiExternalLink} ml={2} />
-          </chakra.a>
-        </Box>
-      </Flex>
-    </SimpleGrid>
+      </Box>
+    );
+  };
+
+  return (
+    <Flex
+      bg="#edf3f8"
+      _dark={{
+        bg: "#3e3e3e",
+      }}
+      p={20}
+      w="auto"
+      justifyContent="center"
+      alignItems="center"
+      
+    >
+      <SimpleGrid
+      rounded="30px"
+        columns={{
+          base: 1,
+          md: 2,
+          lg: 3,
+        }}
+        spacing={20}
+        px={{
+          base: 4,
+          lg: 16,
+          xl: 24,
+        }}
+        py={20}
+        mx="auto"
+        bg="white"
+        _dark={{
+          bg: "gray.800",
+        }}
+        shadow="xl"
+      >
+        <Feature
+          title="Unify your payments stack"
+          icon={
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            />
+          }
+        >
+          Manage all your online and offline sales in one place with a single
+          integration, simplifying reporting and reconciliation. Terminal works
+          seamlessly with Payments, Connect, and Billing.
+        </Feature>
+
+        <Feature
+          title="Own your in-store experience"
+          icon={
+            <path
+              fillRule="evenodd"
+              d="M7 2a1 1 0 00-.707 1.707L7 4.414v3.758a1 1 0 01-.293.707l-4 4C.817 14.769 2.156 18 4.828 18h10.343c2.673 0 4.012-3.231 2.122-5.121l-4-4A1 1 0 0113 8.172V4.414l.707-.707A1 1 0 0013 2H7zm2 6.172V4h2v4.172a3 3 0 00.879 2.12l1.027 1.028a4 4 0 00-2.171.102l-.47.156a4 4 0 01-2.53 0l-.563-.187a1.993 1.993 0 00-.114-.035l1.063-1.063A3 3 0 009 8.172z"
+              clipRule="evenodd"
+            />
+          }
+        >
+          Provide a seamless customer experience across channels, like reserving
+          online and picking up in store. Our SDKs make it easy to integrate
+          into your own mobile and web applications to accept in-store payments.
+        </Feature>
+
+        <Feature
+          title="Grow your platform’s revenue"
+          icon={
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M8 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-2m-4-1v8m0 0l3-3m-3 3L9 8m-5 5h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 00.707.293h3.172a1 1 0 00.707-.293l2.414-2.414a1 1 0 01.707-.293H20"
+            />
+          }
+        >
+          Add in-person payments to your platform or marketplace. Using Terminal
+          with Connect, you can onboard users for online and in-person payments,
+          and unify their payouts across channels.
+        </Feature>
+      </SimpleGrid>
+    </Flex>
   );
 };
-
-export default CTA;
