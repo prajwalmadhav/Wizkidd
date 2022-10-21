b98a627d896ee8ce5957994f4a496b88d6fe791a
PG <prajwalsmadhav@gmail.com>
2022-10-22 01:18:27 +0530

    updated pages for catalog

---
+++ src/pages/Dashboard/Coursecatalog.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Catalog from '../../components/Courses/Catalog'
+
+
+export default function courseCatalog() {
+  return (
+    <>
+        <Catalog />
+    </>
+    
+  )
+}
